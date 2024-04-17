a4bec76c052e12eba7e940d708413bd6b09c7280
Zaid Kotbi <zkotbi@e3r2p15.1337.ma>
2024-04-17 11:07:49 +0100

    file init

--- philo/philo.c
+++ philo/philo.c
@@ -5,19 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zkotbi <hibenouk@1337.ma>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/15 20:17:22 by zkotbi            #+#    #+#             */
-/*   Updated: 2024/04/16 22:01:30 by zkotbi           ###   ########.fr       */
+/*   Created: 2024/04/17 10:30:57 by zkotbi            #+#    #+#             */
+/*   Updated: 2024/04/17 11:05:22 by zkotbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include <unistd.h>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	if (argc < 5 || argc > 6)
-		return (write(2, "invalid arguments\n", 18));
-	if (is_valid_input(argv) == 0)
-		return (write(2, "invalid arguments\n", 18));
-	init_philo(argv);
+		return (write(2, "invalid arguments\n", 18), 1);
+	if (argv != NULL)
+		printf("test\n");
+	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: zkotbi <hibenouk@1337.ma>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 20:21:36 by zkotbi            #+#    #+#             */
-/*   Updated: 2024/04/16 20:40:43 by zkotbi           ###   ########.fr       */
+/*   Updated: 2024/04/17 10:11:54 by zkotbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 #include <pthread.h>
 #include <unistd.h>
 
+// typedef struct s_table {
+// 	
+// } t_table;
 int is_valid_input(char **argv);
 
 #endif
